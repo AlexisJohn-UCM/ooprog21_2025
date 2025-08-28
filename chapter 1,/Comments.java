5ce22e468c88b9bb1a1447b4420648d4b7624981
AlexisJohn-UCM <alexisjohncarvajal@yahoo.com>
2025-08-28 08:30:39 -0800

    Add files via upload

---
+++ chapter 1,/Comments.java
@@ -0,0 +1,17 @@
+/**
+ * This is a comment.
+ * 
+ */
+
+public class Comments {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("Hello, comments!");
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         */
+        System.out.println("This program demonstrates all 3 types of comments.");
+    }
+}
---
+++ chapter 1,/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("And I need you,");
+        System.out.println("And I miss you,");
+        System.out.println("And now I wonder,");
+        System.out.println("If I could fall into the sky.");
+    }
+}
